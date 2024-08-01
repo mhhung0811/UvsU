a1002746eedd0f6f9c4743c5f0e40fc8d58d3bb3
OnionSm <vuongha16104@gmail.com>
2024-08-01 21:45:35 +0700

    add wall and trigger for wall, trigger for gate

---
+++ Assets/Scripts/Wall/TriggerWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class TriggerWall : MonoBehaviour
+{
+    // Danh sách các wall sẽ được nhận trigger
+    [SerializeField] private List<GameObject> _walls;
+    
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Has trigger checkpoint");
+        if(collision.gameObject.layer == 7)
+        {
+            foreach(GameObject wall in _walls)
+            {
+                wall.SetActive(!wall.activeSelf);
+            }
+        }
+    }
+
+}
